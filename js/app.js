591c6fe82ca00489227548188003c65ea02f643c
MatthewChong <chiongmatthew96@gmail.com>
2023-02-26 18:05:31 +0800

    height

--- js/app.js
+++ js/app.js
@@ -69,7 +69,7 @@ const nextStep = () => {
     stepsConHeight();
     console.log("HI");
   } else {
-    stepsContainer.style.minHeight = "550px";
+    stepsContainer.style.minHeight = "600px";
   }
   chosenPlan();
   chosenAddOns();
